28897fdf8a860c1e96fe8bb48edd0d5076e961ac
Nhi <45851291+phuongnhi99@users.noreply.github.com>
2020-10-29 21:23:09 +0700

    update views

--- TourDuLich/routes/web.php
+++ TourDuLich/routes/web.php
@@ -19,6 +19,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[
     'as'=>'index',
-    'uses'=>'PageController@getIndex'
+    'uses'=>'IndexController@index'
 ]);
 
+Route::get('/booktour',[
+    'as'=>'booktour',
+    'uses'=>'BookTourController@index'
+]);
+
+Route::get('/tour',[
+    'as'=>'tour',
+    'uses'=>'TourController@index'
+]);
+
+Route::get('/group',[
+    'as'=>'group',
+    'uses'=>'GroupController@index'
+]);
+
+Route::get('/cost',[
+    'as'=>'cost',
+    'uses'=>'CostController@index'
+]);
+
+Route::get('/price',[
+    'as'=>'price',
+    'uses'=>'PriceController@index'
+]);
+
+Route::get('/customer',[
+    'as'=>'customer',
+    'uses'=>'CustomerController@index'
+]);
+
+Route::get('/staff',[
+    'as'=>'staff',
+    'uses'=>'StaffController@index'
+]);
