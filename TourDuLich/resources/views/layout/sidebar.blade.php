1b69a24a56628f63d38171fe1896b8811917260c
beoxinhdep <lyngocmyphuong@gmail.com>
2020-11-04 23:53:17 +0700

    change url sidebar

--- TourDuLich/resources/views/layout/sidebar.blade.php
+++ TourDuLich/resources/views/layout/sidebar.blade.php
@@ -33,7 +33,7 @@
             <a class="collapse-item" href="{{ url('price') }}">Giá Tour</a>
           </div>
         </div>
-      </li>
+      </li>git
 
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
